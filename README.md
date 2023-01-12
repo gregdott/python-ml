cb35a677d82becc475708a4300adf83ef1654e25
gregdott <dott.greg@gmail.com>
2023-01-12 17:44:39 +0200

    Linear regression

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Repo for machine learning stuff written in Python
 This was only just recently created, so don't expect much just yet...
 
 Code that ends up here will be related to my learning about various things in Machine Learning, starting with ultra-basic stuff like linear regression.
+
+I will do my best to ensure that there are raw Python files for each of the Jupyter Notebook files provided. 
---
+++ linear regression/least_squares.py
@@ -0,0 +1,41 @@
+# This is just a basic thing for calculating the trend line for a linear regression
+# There are tools that do that for you, but I wanted to understand the internals
+# of what is actually going on when we calculate that line for a given set of data
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+df = pd.read_csv(dir_path + "/housing.data", delim_whitespace=True, header=None)
+col_name = ['CRIM', 'ZN' , 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.columns = col_name
+#df.head()
+
+test = df[['LSTAT', 'MEDV']]
+tuples = list(test.itertuples(index=False, name=None))
+n = len(tuples)
+sx = 0 # sum of x values
+sy = 0 # sum of y values
+sxs = 0 # sum of x squared values
+sxy = 0 # sum of x*y values
+
+for i in range(n):
+    sx += tuples[i][0]
+    sy += tuples[i][1]
+    sxs += np.power(tuples[i][0], 2)
+    sxy += tuples[i][0]*tuples[i][1]
+
+a = ((n*sxy) - (sx*sy))/((n*sxs) - (sx*sx))
+b = (sy - (a*sx))/n
+
+plt.scatter(*zip(*tuples))
+
+axes = plt.gca()
+x_vals = np.array(axes.get_xlim())
+y_vals = b + a * x_vals
+plt.plot(x_vals, y_vals, '--')
+
+plt.show()
